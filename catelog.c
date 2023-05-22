9c1e0884afafc197296d79fbb136a57490067e46
AROMAL PRAKASH K V <86295944+AROMALPRAKASHKV@users.noreply.github.com>
2023-05-22 21:20:58 +0530

    Update catelog.c

--- catelog.c
+++ catelog.c
@@ -1,62 +1,149 @@
 #include <stdio.h>
-#define PRODUCT_A_PRICE 20
-#define PRODUCT_B_PRICE 40
-#define PRODUCT_C_PRICE 50
+
+#define PRODUCT_COUNT 3
+#define SHIPPING_RATE 5
 #define GIFT_WRAP_FEE 1
-#define SHIPPING_PACKAGE_SIZE 10
-#define SHIPPING_FEE 5
-int main() {
-    int productAQuantity, productBQuantity, productCQuantity;
-    int productAGiftWrap, productBGiftWrap, productCGiftWrap;
-    int totalQuantity, subtotal, shippingFee, giftWrapFee, totalAmount;
-    int discountAmount = 0;
-    char discountName[30];
 
-// Input product quantities and gift wrap options
-    printf("Enter the quantity of Product A: ");
-    scanf("%d", &productAQuantity);
-    printf("Is Product A wrapped as a gift? (0 for no, 1 for yes): ");
-    scanf("%d", &productAGiftWrap);
+struct Product {
+    char name;
+    int price;
+    int quantity;
+    int isGiftWrapped;
+};
 
-    printf("Enter the quantity of Product B: ");
-    scanf("%d", &productBQuantity);
-    printf("Is Product B wrapped as a gift? (0 for no, 1 for yes): ");
-    scanf("%d", &productBGiftWrap);
+struct Discount {
+    char name[20];
+    float discountPercentage;
+    int isApplicable;
+};
 
-    printf("Enter the quantity of Product C: ");
-    scanf("%d", &productCQuantity);
-    printf("Is Product C wrapped as a gift? (0 for no, 1 for yes): ");
-    scanf("%d", &productCGiftWrap);
+void applyDiscount(struct Product *products, struct Discount *discount, int cartTotal) {
+    int i;
+    float maxDiscount = 0;
+    int index = -1;
 
-// Calculate total quantity and subtotal
-    totalQuantity = productAQuantity + productBQuantity + productCQuantity;
-    subtotal = (productAQuantity * PRODUCT_A_PRICE) + (productBQuantity * PRODUCT_B_PRICE) + (productCQuantity * PRODUCT_C_PRICE);
-
- // Apply discount rules
-    if (subtotal > 200) {
-        discountAmount = 10;
-        strcpy(discountName, "flat_10_discount");
-    } else if (productAQuantity > 10 || productBQuantity > 10 || productCQuantity > 10) {
-        int maxQuantity = productAQuantity;
-        if (productBQuantity > maxQuantity)
-            maxQuantity = productBQuantity;
-        if (productCQuantity > maxQuantity)
-            maxQuantity = productCQuantity;
-
-        discountAmount = (maxQuantity * PRODUCT_A_PRICE + maxQuantity * PRODUCT_B_PRICE + maxQuantity * PRODUCT_C_PRICE) * 0.05;
-        strcpy(discountName, "bulk_5_discount");
-    } else if (totalQuantity > 20) {
-        discountAmount = subtotal * 0.1;
-        strcpy(discountName, "bulk_10_discount");
-    } else if (totalQuantity > 30 && (productAQuantity > 15 || productBQuantity > 15 || productCQuantity > 15)) {
-        int excessQuantity = (productAQuantity > 15 ? productAQuantity - 15 : 0) +
-                            (productBQuantity > 15 ? productBQuantity - 15 : 0) +
-                            (productCQuantity > 15 ? productCQuantity - 15 : 0);
-
-        discountAmount = (excessQuantity * PRODUCT_A_PRICE + excessQuantity * PRODUCT_B_PRICE + excessQuantity * PRODUCT_C_PRICE) * 0.5;
-        strcpy(discountName, "tiered_50_discount");
+    for (i = 0; i < PRODUCT_COUNT; i++) {
+        if (discount[i].isApplicable) {
+            float discountAmount = products[i].price * products[i].quantity * discount[i].discountPercentage;
+            if (discountAmount > maxDiscount) {
+                maxDiscount = discountAmount;
+                index = i;
+            }
+        }
     }
 
- // Calculate shipping fee and gift wrap fee
-    shippingFee = (totalQuantity / SHIPPING_PACKAGE_SIZE) * SHIPPING_FEE;
-    giftWrapFee = (productAGiftWrap * productAQuantity + productBGiftWrap * productBQuantity + productCGiftWrap * productCQuantity) * GIFT_WRAP_FEE
+    if (index != -1) {
+        printf("Discount Applied: %s\n", discount[index].name);
+        printf("Discount Amount: $%.2f\n", maxDiscount);
+        cartTotal -= maxDiscount;
+    }
+}
+
+int main() {
+    struct Product products[PRODUCT_COUNT];
+    struct Discount discounts[PRODUCT_COUNT] = {
+        {"flat_10_discount", 0.10, 0},
+        {"bulk_5_discount", 0.05, 0},
+        {"bulk_10_discount", 0.10, 0}
+    };
+
+    int i, cartTotal = 0;
+
+    // Input product details
+    for (i = 0; i < PRODUCT_COUNT; i++) {
+        printf("Enter quantity for product %c: ", 'A' + i);
+        scanf("%d", &products[i].quantity);
+        printf("Is product %c gift wrapped? (1 for yes, 0 for no): ", 'A' + i);
+        scanf("%d", &products[i].isGiftWrapped);
+
+        switch (i) {
+            case 0:
+                products[i].name = 'A';
+                products[i].price = 20;
+                break;
+            case 1:
+                products[i].name = 'B';
+                products[i].price = 40;
+                break;
+            case 2:
+                products[i].name = 'C';
+                products[i].price = 50;
+                break;
+        }
+
+        cartTotal += products[i].price * products[i].quantity;
+    }
+
+    //discount
+    if (cartTotal > 200) {
+        discounts[0].isApplicable = 1;
+    }
+
+    for (i = 0; i < PRODUCT_COUNT; i++) {
+        if (products[i].quantity > 10) {
+            discounts[1].isApplicable = 1;
+            break;
+        }
+    }
+
+    int totalQuantity = 0;
+    for (i = 0; i < PRODUCT_COUNT; i++) {
+        totalQuantity += products[i].quantity;
+    }
+
+    if (totalQuantity > 20) {
+        discounts[2].isApplicable = 1;
+    }
+
+    if (totalQuantity > 30) {
+        for (i = 0; i < PRODUCT_COUNT; i++) {
+            if (products[i].quantity > 15) {
+                discounts[i].isApplicable = 1;
+            }
+        }
+    }
+
+    //giving discount
+    applyDiscount(products, discounts, cartTotal);
+
+    // Calculate shipping fee and gift wrap fee
+    int totalUnits = 0;
+    int shippingPackages = 0;
+    int giftWrapFee = 0;
+
+    for (i = 0; i < PRODUCT_COUNT; i++) {
+        totalUnits += products[i].quantity;
+
+        if (products[i].isGiftWrapped) {
+            giftWrapFee += products[i].quantity;
+        }
+    }
+
+    shippingPackages = totalUnits / 10;
+    if (totalUnits % 10 != 0) {
+        shippingPackages++;
+    }
+
+    int shippingFee = shippingPackages * SHIPPING_RATE;
+
+    //subtotal
+    int subTotal = cartTotal + shippingFee + giftWrapFee;
+
+    //total
+    int total = subTotal;
+
+    printf("\n--- Order Summary ---\n");
+
+    for (i = 0; i < PRODUCT_COUNT; i++) {
+        printf("Product: %c\n", products[i].name);
+        printf("Quantity: %d\n", products[i].quantity);
+        printf("Total Amount: $%d\n", products[i].price * products[i].quantity);
+    }
+
+    printf("Subtotal: $%d\n", subTotal);
+    printf("Shipping Fee: $%d\n", shippingFee);
+    printf("Gift Wrap Fee: $%d\n", giftWrapFee);
+    printf("Total: $%d\n", total);
+
+    return 0;
+}
